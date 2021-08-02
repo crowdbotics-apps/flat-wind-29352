313d3feaee0702f05e6ccb519acef99f1710ce3b
Crowdbotics <team@crowdbotics.com>
2021-08-02 22:56:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "flat_wind_29352Identifier",
+  url: "https://flat_wind_29352Identifier.botics.co",
+  api: "https://flat_wind_29352Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
